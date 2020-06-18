b2a0a3850d07cbe7831d99f5023a65b0a8d8510d
Jelle De Clercq <jelle.de.clercq@qite.be>
2020-06-18 12:00:15 +0200

    Made some utils available to import

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 import TreeSelect from "./components/TreeSelect";
+import buildPathMap from "./utils/buildPathMap";
+import hasSelectedChildren from "./utils/hasSelectedChildren";
+import simplifySelection from "./utils/simplifySelection";
+
+export {
+  buildPathMap,
+  hasSelectedChildren,
+  simplifySelection
+};
 
 export default TreeSelect;
